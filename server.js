be90564d37056a7454ac0001612ae4ef1386cec1
Alex Mole <alex.molen@gmail.com>
2021-05-25 11:51:36 +0100

    umm yeah

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ const server = express()
   .use(express.static('static'))
   .listen(PORT, () => console.log(`Listening on ${PORT}`));
 
-const websock = new ws.server({ server });
+const websock = new ws.Server({ server });
 websock.on('connection', ws => {
   console.log('we got one!');
   ws.on('close', () => console.log('they left T_T'));
